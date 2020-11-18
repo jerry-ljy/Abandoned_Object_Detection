b483daf408e586610f77bf24812cb236e5c9f47e
lijieyu <lijieyu125@gmail.com>
2020-11-18 16:23:57 +0800

    req: add deep sort module

--- detector/detector.py
+++ detector/detector.py
@@ -7,13 +7,18 @@ import torch.backends.cudnn as cudnn
 from numpy import random
 
 import sys
+
 sys.path.insert(0, '../yolov5')
 
 from yolov5.models.experimental import attempt_load
 from yolov5.utils.datasets import LoadStreams, LoadImages
-from yolov5.utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords,set_logging
+from yolov5.utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, set_logging
 from yolov5.utils.torch_utils import select_device, load_classifier, time_synchronized
 from yolov5.utils.plots import plot_one_box
+
+from deep_sort.utils.parser import get_config
+from deep_sort.deep_sort import DeepSort
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
 
 class Detector:
     def __init__(self):
@@ -21,9 +26,9 @@ class Detector:
         self.imgsz = 640
         self.conf_thres = 0.4
         self.iou_thres = 0.5
-        self.classes=None
-        self.augment=False
-        self.agnostic_nms=False
+        self.classes = [0,24,25,26,28,39,41,63,64,66,67,73]
+        self.augment = False
+        self.agnostic_nms = False
 
         # Initialize
         set_logging()
@@ -36,9 +41,51 @@ class Detector:
         if self.half:
             self.model.half()  # to FP16
 
+        # initialize deepsort
+        self.cfg = get_config()
+        self.cfg.merge_from_file("../deep_sort/configs/deep_sort.yaml")
+        self.deepsort = DeepSort(self.cfg.DEEPSORT.REID_CKPT,
+                            max_dist=self.cfg.DEEPSORT.MAX_DIST, min_confidence=self.cfg.DEEPSORT.MIN_CONFIDENCE,
+                            nms_max_overlap=self.cfg.DEEPSORT.NMS_MAX_OVERLAP,
+                            max_iou_distance=self.cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                            max_age=self.cfg.DEEPSORT.MAX_AGE, n_init=self.cfg.DEEPSORT.N_INIT, nn_budget=self.cfg.DEEPSORT.NN_BUDGET,
+                            use_cuda=True)
+
+    def bbox_rel(self, image_width, image_height, *xyxy):
+        """" Calculates the relative bounding box from absolute pixel values. """
+        bbox_left = min([xyxy[0], xyxy[2]])
+        bbox_top = min([xyxy[1], xyxy[3]])
+        bbox_w = abs(xyxy[0] - xyxy[2])
+        bbox_h = abs(xyxy[1] - xyxy[3])
+        x_c = (bbox_left + bbox_w / 2)
+        y_c = (bbox_top + bbox_h / 2)
+        w = bbox_w
+        h = bbox_h
+        return x_c, y_c, w, h
+
+    def draw_boxes(self, img, bbox, identities=None, offset=(0, 0)):
+        for i, box in enumerate(bbox):
+            x1, y1, x2, y2 = [int(i) for i in box]
+            x1 += offset[0]
+            x2 += offset[0]
+            y1 += offset[1]
+            y2 += offset[1]
+            # box text and bar
+            id = int(identities[i]) if identities is not None else 0
+            color = [int((p * (id ** 2 - id + 1)) % 255) for p in palette]
+            color = tuple(color)
+            label = '{}{:d}'.format("", id)
+            t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+            cv2.rectangle(img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
+            cv2.putText(img, label, (x1, y1 + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+        return img
+
+
     def detect(self, source):
         webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
             ('rtsp://', 'rtmp://', 'http://'))
+
 
         # Set Dataloader
         vid_path, vid_writer = None, None
@@ -70,7 +117,8 @@ class Detector:
             pred = self.model(img, augment=self.augment)[0]
 
             # Apply NMS
-            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes,
+                                       agnostic=self.agnostic_nms)
             t2 = time_synchronized()
 
             # Process detections
@@ -85,14 +133,47 @@ class Detector:
                     det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
                     # Write results
-                    for *xyxy, conf, cls in reversed(det):
-                        label = '%s %.2f' % (names[int(cls)], conf)
-                        # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-                        location = [xyxy[i].cpu().numpy().tolist() for i in range(4)]
+                    # for *xyxy, conf, cls in reversed(det):
+                    #     label = '%s %.2f' % (names[int(cls)], conf)
+                    #     plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                    #     location = [xyxy[i].cpu().numpy().tolist() for i in range(4)]
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                    bbox_xywh = []
+                    confs = []
+                    clss = []
+
+                    # Adapt detections to deep sort input format
+                    for *xyxy, conf, cls in det:
+                        img_h, img_w, _ = im0.shape
+                        x_c, y_c, bbox_w, bbox_h = self.bbox_rel(img_w, img_h, *xyxy)
+                        obj = [x_c, y_c, bbox_w, bbox_h]
+                        bbox_xywh.append(obj)
+                        confs.append([conf.item()])
+                        clss.append(names[int(cls)])
+
+                    xywhs = torch.Tensor(bbox_xywh)
+                    confss = torch.Tensor(confs)
+
+                    #####
+                    # Pass detections to deepsort
+                    outputs = self.deepsort.update(xywhs, confss, im0)
+
+                    # draw boxes for visualization
+                    if len(outputs) > 0:
+                        bbox_xyxy = outputs[:, :4]
+                        identities = outputs[:, -1]
+                        self.draw_boxes(im0, bbox_xyxy, identities)
+
+
                 print('%sDone. (%.3fs)' % (s, t2 - t1))
-                # cv2.imshow(str(p), im0)
-                # if cv2.waitKey(1) == ord('q'):  # q to quit
-                #     raise StopIteration
+                cv2.imshow(str(p), im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
         print('Done. (%.3fs)' % (time.time() - t0))
 
 
---
+++ track.py
@@ -0,0 +1,1 @@
+from detector.detector import Detector
